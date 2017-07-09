5596dc8379020bc0efcb4f38f04443b07392f0a4
parkjaeseong <tmakdlfwotjd@naver.com>
2017-07-09 18:55:00 +0900

    Please merge pickeep

--- Merge_one/filepicker/src/main/java/droidninja/filepicker/PickerManagerListener.java
+++
@@ -1,8 +0,0 @@
-package droidninja.filepicker;
-
-import java.util.ArrayList;
-
-public interface PickerManagerListener{
-        void onItemSelected(int count);
-        void onSingleItemSelected(ArrayList<String> paths);
-    }
---
+++ pickeep(final_v1)/app/src/androidTest/java/vi/filepicker/EnterActivityTest.java
@@ -0,0 +1,11 @@
+package vi.filepicker;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by 박재성 on 2017-06-13.
+ */
+
+public class EnterActivityTest extends TestCase {
+
+}
